b3e5941cd8d9c9c4bb15c4d181d1ae73f36bf153
OraceMundle <71685178+OraceMundle@users.noreply.github.com>
2021-09-01 07:14:30 -0500

    Update CustomerJDBCImpl.java

--- src/main/java/Services/JDBCImpl/CustomerJDBCImpl.java
+++ src/main/java/Services/JDBCImpl/CustomerJDBCImpl.java
@@ -8,6 +8,8 @@ package Services.JDBCImpl;
 import Domain.Customer;
 import Services.ICustomerService;
 import Services.IService;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
 
@@ -63,7 +65,7 @@ public class CustomerJDBCImpl extends JDBCMainConfiguration implements ICustomer
     @Override
     public void addCustomerJDBC(Customer customer) throws Exception {
         
-     String insertCustomer = "INSERT INTO customer(trn, f_name, l_name, email, telephone_nmunber, dob) "
+     String insertCustomer = "INSERT INTO customer(trn, f_name, l_name, email, telephone_number, dob) "
                 + "values('" + customer.getTrn() + 
                 "', '" + customer.getFirstname() +
                 "', '" + customer.getLastname() + 
@@ -83,9 +85,10 @@ public class CustomerJDBCImpl extends JDBCMainConfiguration implements ICustomer
     @Override
     public void updateCustomerJDBC(Customer customer) throws Exception {
          
-        String updateCustomer = "UPDATE authors SET Title = '" + customer.getTrn() + 
+        String updateCustomer = "UPDATE customer SET trn = '" + customer.getTrn() + 
                 "', '" + customer.getFirstname() +
                 "', '" + customer.getLastname() + 
+                "', '" + customer.getEmail() +
                 "', '" + customer.getTelephoneNum() + 
                 "', '" + customer.getDob()+"')";    
           
@@ -103,19 +106,45 @@ public class CustomerJDBCImpl extends JDBCMainConfiguration implements ICustomer
 
     @Override
     public Customer geCustomerJDBC(String trn) throws Exception {
+            String selectCustomer = "Select * From authors Where ID = " + trn;
+        statement=this.getConnection().createStatement();
         
+        ResultSet rs=statement.executeQuery(selectCustomer);
+        
+        Customer customer=new Customer();
+        customer.setTrn(trn);
+        customer.setFirstname(rs.getString("f_name"));
+        customer.setLastname(rs.getString("l_name"));
+        customer.setEmail(rs.getString("email"));
+        customer.setTelephoneNum(rs.getString("telephone_number"));
+        customer.setDob(rs.getString("dob"));
+        
+        return customer;
 
 
     }
 
     @Override
     public List<Customer> getAllCustomersJDBC() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         ResultSet rs=null;
+        PreparedStatement ps;
+        
+        String SelectAll="Select * From customer";
+        //statement=this.getConnection().createStatement();
+        ps=this.getConnection().prepareStatement(SelectAll, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        rs=ps.executeQuery(SelectAll);
+        
+        
+        return (List<Customer>) rs;
+          
     }
+    
+    
 
     @Override
     public void deleteCustomerJDBC(String trn) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        statement=this.getConnection().createStatement();
+        statement.execute("Delete From authors Where ID = " + trn);
     }
     
 }
